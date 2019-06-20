f5ad587d84bef77a320335826b7392e1bca56172
Amy Westcott <amy.westcott@yahoo.com>
2019-06-20 20:53:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster, :grade, :stundent 
+  def initialize(name)
+    @name = name
+    end 
+  def roster(roster = {})
+    @roster = roster 
+     end
+   def add_student(student, grade)
+    roster
+    if @roster.keys.include?(grade)
+      @roster[grade] << student 
+    else 
+      @roster = {grade => []}
+      @roster[grade] << student 
+    end 
+    @roster
+  end 
+  def grade(grade)
+  end
+  def sort
+  end
+end
